c5109e5028d542ba588d4e5cdf48289584b9849f
Szymon "l7ssha" Uglis <szymon.uglis@hotmail.com>
2016-05-05 14:51:11 +0200

    Box2D added

--- NinjaPlatformer/GameplayScreen.h
+++ NinjaPlatformer/GameplayScreen.h
@@ -1,33 +1,47 @@
 #pragma once
 
-#include <iostream>
-#include <SDL/SDL.h>
-
-#include <PragmaEngine/IMainGame.h>
+#include "Box.h"
 #include <PragmaEngine/IGameScreen.h>
+#include <Box2D/Box2D.h>
+#include <vector>
+#include <PragmaEngine/SpriteBatch.h>
+#include <PragmaEngine/GLSLProgram.h>
+#include <PragmaEngine/Camera2D.h>
+#include <PragmaEngine/GLTexture.h>
+#include <PragmaEngine/Window.h>
 
 // Our custom gameplay screen that inherits from the IGameScreen
 class GameplayScreen : public PragmaEngine::IGameScreen {
 public:
-	GameplayScreen();
-	~GameplayScreen();
+    GameplayScreen(PragmaEngine::Window* window);
+    ~GameplayScreen();
 
-	virtual int getNextScreenIndex() const override;
+    virtual int getNextScreenIndex() const override;
 
-	virtual int getPreviousScreenIndex() const override;
+    virtual int getPreviousScreenIndex() const override;
 
-	virtual void build() override;
+    virtual void build() override;
 
-	virtual void destroy() override;
+    virtual void destroy() override;
 
-	virtual void onEntry() override;
+    virtual void onEntry() override;
 
-	virtual void onExit() override;
+    virtual void onExit() override;
 
-	virtual void update() override;
+    virtual void update() override;
 
-	virtual void draw() override;
+    virtual void draw() override;
 
 private:
-	void checkInput();
+    void checkInput();
+
+	PragmaEngine::SpriteBatch m_spriteBatch;
+	PragmaEngine::GLSLProgram m_textureProgram;
+	PragmaEngine::Camera2D m_camera;
+	PragmaEngine::GLTexture m_texture;
+	PragmaEngine::Window* m_window;
+
+    std::vector<Box> m_boxes;
+    std::unique_ptr<b2World> m_world;
 };
+
--- PragmaEngine/AudioEngine.h
+++ PragmaEngine/AudioEngine.h
@@ -13,11 +13,8 @@ namespace PragmaEngine {
 		AudioEngine();
 		~AudioEngine();
 
-		int musicindex = 0;
-
 		void playMusic(const std::string filepath, float volume);
 		void playMusic(const std::string filepath, bool looped, bool startPaused, float volume);
-
 		void playMusicfromplaylist(std::vector<std::string>& playlist, float volume);
 
 		void playSound(const std::string filepath, float volume);
@@ -27,6 +24,8 @@ namespace PragmaEngine {
 		void resumeMusic();
 		void stopMusic();
 	private:
+		int musicindex = 0;
+
 		irrklang::ISoundEngine* musicEngine;
 		irrklang::ISoundEngine* soundEngine;
 	};
